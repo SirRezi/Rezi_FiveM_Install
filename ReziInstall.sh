2853e8b9dcd10949ace5bf02977d8a5658e49799
SirRezi <138231740+SirRezi@users.noreply.github.com>
2024-01-02 07:52:46 +0100

    Update ReziInstall.sh

--- ReziInstall.sh
+++ ReziInstall.sh
@@ -8,14 +8,13 @@ update_script() {
     echo -e "${YELLOW}Aktualisiere das Skript...${NC}"
     repo="SirRezi/Rezi_FiveM_Install"
     script="ReziInstall.sh"
-    curl -sLJO "https://raw.githubusercontent.com/$repo/main/$script"
+    curl -sLO "https://raw.githubusercontent.com/$repo/main/$script"
     chmod +x $script
     ./$script
     exit 0
 }
 
 check_for_update() {
-
     echo -e "${YELLOW}Überprüfe auf Updates...${NC}"
     repo="SirRezi/Rezi_FiveM_Install"
     script="ReziInstall.sh"
@@ -36,40 +35,3 @@ check_for_update() {
 }
 
 check_for_update
-
-echo -e "${YELLOW}=====================================================${NC}"
-echo -e "${YELLOW}============ FiveM mit TxAdmin Installer ============${NC}"
-echo -e "${YELLOW}=====================================================${NC}"
-echo -e "${YELLOW}Dieses Skript wurde von SirRezi erstellt.${NC}"
-echo
-
-read -p "Möchtest du die Installation von FiveM mit TxAdmin starten? (ja/nein): " choice
-
-if [[ "$choice" != "ja" ]]; then
-    echo -e "${GREEN}Installation abgebrochen.${NC}"
-    exit 0
-fi
-
-echo -e "${YELLOW}Aktualisiere Paketliste und installiere benötigte Pakete...${NC}"
-apt update
-apt upgrade -y
-apt-get install -y xz-utils git screen
-
-echo -e "${YELLOW}Erstelle FiveM-Server-Verzeichnis und lade FiveM-Server herunter...${NC}"
-mkdir -p /home/FiveM/server
-cd /home/FiveM/server
-wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/7257-030778a167242f79e0f59cd8d9c04b970e3b75c2/fx.tar.xz
-tar xf fx.tar.xz
-rm fx.tar.xz
-
-echo -e "${YELLOW}Erstelle FiveM-Server-Daten-Verzeichnis und konfiguriere...${NC}"
-mkdir -p /home/FiveM/server-data
-cd /home/FiveM/server-data
-git clone https://github.com/citizenfx/cfx-server-data.git /home/FiveM/server-data
-
-echo -e "${YELLOW}=====================================================${NC}"
-echo -e "${YELLOW}============ Installation ist fertig ================${NC}"
-echo -e "${YELLOW}=====================================================${NC}"
-echo
-
-exit 0
