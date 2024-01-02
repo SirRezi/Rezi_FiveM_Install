38e7781ab63e6f4922f76250da416ca3fe61e013
SirRezi <138231740+SirRezi@users.noreply.github.com>
2024-01-02 07:53:58 +0100

    Update ReziInstall.sh

--- ReziInstall.sh
+++ ReziInstall.sh
@@ -28,10 +28,46 @@ check_for_update() {
             update_script
         else
             echo -e "${YELLOW}Du hast das Update abgelehnt. Das Skript ist auf dem neuesten Stand.${NC}"
+            exit 0 # Hier wird das Skript beendet, wenn ein Update abgelehnt wird
         fi
     else
         echo -e "${YELLOW}Das Skript ist auf dem neuesten Stand.${NC}"
     fi
 }
 
-check_for_update
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e "${YELLOW}============ FiveM mit TxAdmin Installer ============${NC}"
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e "${YELLOW}Dieses Skript wurde von SirRezi erstellt.${NC}"
+echo
+
+read -p "MÃ¶chtest du die Installation von FiveM mit TxAdmin starten? (ja/nein): " choice
+
+if [[ "$choice" != "ja" ]]; then
+    echo -e "${GREEN}Installation abgebrochen.${NC}"
+    exit 0
+fi
+
+echo -e "${YELLOW}Aktualisiere Paketliste und installiere benÃ¶tigte Pakete...${NC}"
+apt update
+apt upgrade -y
+apt-get install -y xz-utils git screen
+
+echo -e "${YELLOW}Erstelle FiveM-Server-Verzeichnis und lade FiveM-Server herunter...${NC}"
+mkdir -p /home/FiveM/server
+cd /home/FiveM/server
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/7257-030778a167242f79e0f59cd8d9c04b970e3b75c2/fx.tar.xz
+tar xf fx.tar.xz
+rm fx.tar.xz
+
+echo -e "${YELLOW}Erstelle FiveM-Server-Daten-Verzeichnis und konfiguriere...${NC}"
+mkdir -p /home/FiveM/server-data
+cd /home/FiveM/server-data
+git clone https://github.com/citizenfx/cfx-server-data.git /home/FiveM/server-data
+
+echo -e "${YELLOW}=====================================================${NC}"
+echo -e "${YELLOW}============ Installation ist fertig ================${NC}"
+echo -e "${YELLOW}=====================================================${NC}"
+echo
+
+exit 0
